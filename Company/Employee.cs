46786e833be292efa27fc54db27b6fc08819459d
Anna Aleksiuk <aal@scalepoint.com>
2023-07-14 15:58:13 +0200

    Add project files.

---
+++ Company/Employee.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Company;
+
+public class Employee
+{
+    public string Name;
+    public string Surname;
+    public int Age;
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Employee name {Name}, surname {Surname} and age {Age}.");
+    }
+}
---
+++ Company/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Company;
+
+Employee employee1 = new Employee();
+employee1.Name = "Nowak";
+employee1.Surname = "Anna";
+employee1.Age = 56;
+employee1.PrintInfo();
+
+Employee employee2 = new Employee();
+employee2.Name = "Kowalski";
+employee2.Surname = "Marek";
+employee2.Age = 60;
+employee2.PrintInfo();
+
+Employee employee3 = new Employee();
+employee3.Name = "Wolak";
+employee3.Surname = "Adam";
+employee3.Age = 25;
+employee3.PrintInfo();
